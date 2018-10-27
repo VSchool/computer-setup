73f36c80f5abe4f3b83d678193c69c6a64e7d427
Bob Ziroll <elderziroll@gmail.com>
2018-10-27 00:16:44 -0600

    Made cleanup script safer and added better formatting to setup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,52 +1,61 @@
 #!/bin/bash
-brew uninstall nvm
-brew uninstall bash-completion
-rm -rf ~/.ssh/
-rm -rf ~/.nvm/
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-> ~/.bash_profile
 
-if [ -d /usr/local/var ]
+echo "You should only run this script on a test computer if you're okay with lots of stuff being deleted"
+echo "You're about to uninstall lots of things and delete a bunch of folders, including the dev folder."
+read -p "Are you sure you're okay with this? Type 'please clean up everything' to confirm" answer
+if [[ ${answer} != "please clean up everything" ]]
 then
-    sudo rm -rf /usr/local/var
-fi
+    echo "Canceling the script now. Nothing will be uninstall or deleted."
+    return 1
+else
+    brew uninstall nvm
+    #brew uninstall bash-completion
+    rm -rf ~/.ssh/
+    rm -rf ~/.nvm/
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+    #> ~/.bash_profile
 
-if [ -d /usr/local/share ]
-then
-    sudo rm -rf /usr/local/share
-fi
+    if [ -d /usr/local/var ]
+    then
+        sudo rm -rf /usr/local/var
+    fi
 
-if [ -d ~/.git-completion.bash ]; 
-then
-    sudo rm -rf ~/.git-completion.bash
-fi
+    if [ -d /usr/local/share ]
+    then
+        sudo rm -rf /usr/local/share
+    fi
 
-# Clean up dev folder
-if [ -d ~/dev ]
-then
-    sudo rm -rf ~/dev
-fi
+    if [ -d ~/.git-completion.bash ];
+    then
+        sudo rm -rf ~/.git-completion.bash
+    fi
 
-# Clean up Xcode
-if [ -d /Library/Developer/CommandLineTools ]
-then
-    sudo rm -rf /Library/Developer/CommandLineTools
-fi
+    # Clean up dev folder
+    if [ -d ~/dev ]
+    then
+        sudo rm -rf ~/dev
+    fi
 
-if [ -d /Applications/Xcode.app ]
-then
-    sudo /Developer/Library/uninstall-devtools --mode=all
-    sudo rm -rf /Applications/Xcode.app/
-fi
+    # Clean up Xcode
+    if [ -d /Library/Developer/CommandLineTools ]
+    then
+        sudo rm -rf /Library/Developer/CommandLineTools
+    fi
 
-if [ -d ~/Library/Developer ]
-then
-    sudo rm -rf ~/Library/Developer
-fi
+    if [ -d /Applications/Xcode.app ]
+    then
+        sudo /Developer/Library/uninstall-devtools --mode=all
+        sudo rm -rf /Applications/Xcode.app/
+    fi
 
-if [ -d ~/Library/Caches/com.apple.dt.Xcode ]
-then
-    sudo rm -rf ~/Library/Caches/com.apple.dt.Xcode/*
-fi
+    if [ -d ~/Library/Developer ]
+    then
+        sudo rm -rf ~/Library/Developer
+    fi
 
+    if [ -d ~/Library/Caches/com.apple.dt.Xcode ]
+    then
+        sudo rm -rf ~/Library/Caches/com.apple.dt.Xcode/*
+    fi
+fi
 osascript -e "do shell script \"osascript -e \\\"tell application \\\\\\\"Terminal\\\\\\\" to quit\\\" &> /dev/null &\""; exit
--- vschool-mac-setup.sh
+++ vschool-mac-setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-lightblue=$'\e[30;104m'
-#alerttext="\e[30;41m"
-#alerttext="\e[1;37;42m"
+
+lightblue=$'\e[97;104m'
 clear=$'\e[0m'
 newline=$'\n'
+
+if [[ $(uname -s) != Darwin ]]
+then
+    read -p "${lightblue}Sorry, this script will currently only work on a Mac. Hit return to exit.${clear}"
+    return 1
+fi
+
 
 # Emojis
 tada=$'\xF0\x9F\x8E\x89'
@@ -17,61 +23,63 @@ grimace=$'\xF0\x9F\x98\xAC'
 mac_command_symbol=$'\xC2\x9D'
 sweat_drops=$'\xF0\x9F\x92\xA6'
 
-
+# Intro
 echo -e "${lightblue}${tada} Welcome to V School! ${tada}${clear}"
 sleep 3
-echo -e "${lightblue}I'm going to do some of the basic, mundane setup stuff for you. So let's get started! ${rocket}${clear}"
+echo -e "${lightblue}I'm here to do some of the basic, mundane setup stuff for you. Let's get started! ${rocket}${clear}"
 sleep 3
-echo -e "${lightblue}Throughout this process, it may ask you for your password multiple times${clear}"
+echo -e "${lightblue}Throughout this process, you may be asked to provide your password a few times${clear}"
 sleep 3
 echo -e "${lightblue}When that happens, you'll type in the same password you use to log in to your computer${clear}"
 sleep 3
-echo -e "${lightblue}It will look like nothing is typing, but it is, I promise${clear}"
+echo -e "${lightblue}It will look like nothing is typing, but I promise it is${clear}"
 sleep 3
 echo -e "${lightblue}When you've finished entering your password, press return to continue${clear}"
 sleep 3
-echo -e "${lightblue}One more thing. So I don't talk too fast, I'll let you press the return key to advance through the prompt${clear}"
+echo -e "${lightblue}One more thing... so I don't talk too fast (or slow), I'll let you press the return key to advance through the prompts from here on${clear}"
 sleep 3
 read -p "${lightblue}Try it now ${hourglass} (return)${clear}"
-read -p "${lightblue}Nicely done. You're basically a real hacker already! ${space_invader} (return)${clear}"
-echo -e "${lightblue}I don't actually know your name yet! Please enter your full name (first and last) below. (return)${clear}"
-read first_name last_name
+read -p "${lightblue}Nicely done! You're basically a real hacker already! ${space_invader} (return)${clear}"
+read -p "${lightblue}I don't actually know your name yet! Please enter your full name (first and last):${clear} " first_name last_name
+echo
 
 while [[ -z ${first_name}  ]] || [[ -z ${last_name} ]]
 do
-    echo -e "${lightblue}Whoops, looks like you entered something wrong. Please try again (first and last names): ${clear} "
-    read first_name last_name
+    echo -e "${lightblue}Whoops, looks like you may have entered something incorrectly${clear}"
+    read -p "${lightblue}Please try again. Make sure to type both first AND last names:${clear} " first_name last_name
+    echo
 done
 
 # Todo: Add a check to make sure they're happy with the name they put in
-read -p "${lightblue}Great! Nice to meet you ${first_name} (return)${clear}"
+read -p "${lightblue}Great! Nice to meet you ${first_name}! (return)${clear}"
 
+# Todo: Allow them to keep ZSH if that's what they're using
 # Make sure the default shell is bash
-if [ $(echo $0) != "/bin/bash" ]
+if [[ $(echo $0) != "/bin/bash" ]]
 then
-    echo -e "${lightblue}For simplicity, I'm switching your default shell to bash. If you want to keep another shell, please speak with an instructor for help.${clear}"
+    echo -e "${lightblue}For simplicity, I'm switching your default shell to bash. If you want to switch back to another shell (zsh, e.g.), please speak with an instructor for help.${clear}"
     chsh -s /bin/bash
 #else
 #    echo -e "${lightblue}I just checked if you're using bash for your shell program, and it looks like you are! ${thumbs_up}${clear}"
 fi
 
-# Todo: Turn this into a while loop so they can't move ahead until it's installed
-if [ ! -d /Applications/Google Chrome.app ]
-    then
-        read -p "${lightblue}Before we get too much further, I noticed that it looks like you may not have the Google Chrome browser installed yet. (return)${clear} "
-        read -p "${lightblue}Let's go download it now. Hit return to open a browser to the download page. I'll wait here. ${hourglass} (return)${clear} "
-        /usr/bin/open https://www.google.com/chrome/
-        read -p "${lightblue}Once you've finished installing Chrome, hit return to continue (return)${clear} "
-    fi
+while [[ ! -d "/Applications/Google Chrome.app" ]]
+do
+    read -p "${lightblue}Before we get too much further, I noticed that it looks like you may not have the Google Chrome browser installed yet. (return)${clear} "
+    read -p "${lightblue}Let's go download it now. Hit return to open a browser to the download page. I'll wait here. ${hourglass} (return)${clear} "
+    /usr/bin/open https://www.google.com/chrome/
+    read -p "${lightblue}Once you've finished installing Chrome, hit return to continue. I'll know if you tried to cheat ${grimace} (return)${clear} "
+done
 
 read -p "${lightblue}Now I'll be creating an SSH key for you. (return)${clear}"
-read -p "${lightblue}This is useful for communicating with external services, like GitHub (return)${clear}"
+read -p "${lightblue}This is useful for communicating securely with external services, like GitHub (return)${clear}"
 
 # Check for an existing SSH Key and create a new one if it doesn't exist
 if [ -f ~/.ssh/id_rsa.pub ]
 then
-    echo
-    read -p "${lightblue}Actually, it looks like you've already got an SSH file, which is great! ${thumbs_up} (return)${clear}"
+    echo -e "${lightblue}...${clear}"
+    sleep 1
+    read -p "${lightblue}Actually, it looks like you've already got an SSH key, which is great! ${thumbs_up} (return)${clear}"
 
     # Save public ssh file contents in variable (looking for the comment)
     IN=$( cat ~/.ssh/id_rsa.pub )
@@ -90,14 +98,20 @@ then
     read -p "${lightblue}Okay, for real this time. Hit return to open the GitHub settings page (return)${clear}"
 
     /usr/bin/open https://github.com/settings/emails
-
-    echo -e "${lightblue}Welcome back, ${first_name}!${clear} "
+    echo
+    read -p "${lightblue}Welcome back, ${first_name}!${clear}"
     read -p "${lightblue}So, is ${comment} the actual email address you use for GitHub? Y/n:${clear} " -n 1 response;echo
+    while [[ ! ${response} =~ ^[Yy]$ && ! ${response} =~ ^[Nn]$ ]]
+    do
+        read -p "${lightblue}Whoops, I missed that. Please enter Y or N:${clear} " -n 1 response;echo
+    done
+
     if [[ ${response} =~ ^[Yy]$ ]]
     then
         email=${comment}
         read -p "${lightblue}Great! We can move forward to the next step then! ${rocket} (return)${clear}"
-    else
+    elif [[ ${response} =~ ^[Nn]$ ]]
+    then
         read -p "${lightblue}Okay, no problem. Please enter the email address you use for GitHub:${clear} " email
         ssh-keygen -f ~/.ssh/id_rsa -o -c -C ${email}
         read -p "${lightblue}Alright, I've updated your SSH key to contain the correct email address! (return)${clear}"
@@ -143,30 +157,30 @@ read -p "${lightblue}Next, you'll need to add it to your GitHub SSH keys (return
 read -p "${lightblue}In a minute, I'm going to open the browser again for you (return)${clear} "
 read -p "${lightblue}Here's what you're going to do on your end (it's pretty easy): (return)${clear}"
 read -p "${lightblue}The browser will open to the 'new SSH key' page on GitHub (return)${clear}"
-read -p "${lightblue}For the 'Title' field, put something like 'Bob\'s 2016 Macbook Pro' (return)${clear}"
+read -p "${lightblue}For the \"Title\" field, put something like \"Bob's 2016 Macbook Pro\" (return)${clear}"
 read -p "${lightblue}(You can put anything here that will remind you in the future which computer this SSH key is tied to) (return)${clear}"
-read -p "${lightblue}Then simply paste (${mac_command_symbol} + P) the SSH key (which is already copied for you) into the 'Key' input box and hit the green 'Add SSH key' button (return)${clear}"
+read -p "${lightblue}Then simply paste (⌘ + V) the SSH key (which is already copied for you) into the 'Key' input box and hit the green 'Add SSH key' button (return)${clear}"
 read -p "${lightblue}Make sure to come back here once you've added your SSH key (return)${clear}"
 read -p "${lightblue}Alright, I think you're ready ${rocket} (return)${clear}"
 read -p "${lightblue}Opening the browser now (return)${clear}"
 /usr/bin/open https://github.com/settings/ssh/new
-read -p "${lightblue}(If you happened to get an error that said the SSH key is already in use, you're all set!)${clear}"
-
+echo
+read -p "${lightblue}Welcome back again! How did that go? It wasn't too bad, was it?${clear}"
+read -p "${lightblue}If you happened to get an error that said the SSH key is already in use, that's not a problem and you're all set!)${clear}"
 
 # Install Homebrew
-read -p "${lightblue}How did that go? It wasn't too bad, was it? (return)${clear}"
 read -p "${lightblue}Next we're going to install Homebrew, a package manager for Mac (return)${clear}"
 read -p "${lightblue}Basically it's a great tool for installing developer-y programs to your Mac (return)${clear}"
-read -p "${lightblue}Installing Homebrew may also install the Xcode Command Line Tools, which (FYI) can sometimes take awhile to finish (return)${clear}"
-read -p "${lightblue}Hit return to start the install (return)${clear}"
+read -p "${lightblue}Installing Homebrew may also install the Xcode Command Line Tools, which – FYI – can sometimes take awhile to finish (return)${clear}"
+read -p "${lightblue}Hit return to start the install, then patiently wait until you see the next blue text. ${hourglass} (return)${clear}"
 echo -e "${lightblue}Installing Homebrew...${clear}"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 read -p "${lightblue}Done! Let's move on ${rocket} (return)${clear}"
 # Install and set up NVM
 read -p "${lightblue}Now I'm going to install NVM, which stands for the Node Version Manager (return)${clear}"
-read -p "${lightblue}It makes installing (and switching versions of) Node.js very easy (return)${clear}"
+read -p "${lightblue}It makes installing and managing Node.js very easy (return)${clear}"
 read -p "${lightblue}(It's okay if you're not sure what Node.js is, you'll learn all that in great detail later in the course) (return)${clear}"
-read -p "${lightblue}Hit return to start the install (return)${clear}"
+read -p "${lightblue}Hit return to start the install. Remember to wait until you see the blue text again! (return)${clear}"
 echo -e "${lightblue}Installing NVM...${clear}"
 brew install nvm
 mkdir ~/.nvm
@@ -178,7 +192,7 @@ EOL
 source ~/.bash_profile
 
 # Install the latest stable version of node.js
-read -p "${lightblue}${sweat_smile}${sweat_drops} You're enjoying watching me do all the work, aren't you? (return)${clear}"
+read -p "${lightblue}${sweat_smile} You're enjoying watching me do all the work, aren't you? (return)${clear}"
 read -p "${lightblue}Now I'm going to use NVM to install the latest stable version of Node.js (return)${clear}"
 read -p "${lightblue}Hit return to start the install (return)${clear}"
 echo -e "${lightblue}Installing the latest stable version of Node.js...${clear}"
@@ -224,8 +238,8 @@ echo 'export PS1="\w\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "' >> ~/.bash
 #sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer/
 #sudo xcodebuild -license accept
 
-read -p "${lightblue}${sweat_smile}${sweat_drops} Alright! That's everything! (return)${clear}"
+read -p "${lightblue}${sweat_smile} Alright! That's everything! (return)${clear}"
 read -p "${lightblue}It's been nice working with you, ${first_name}! (return)${clear}"
-read -p "${lightblue}Hit return to close the Terminal app. Once you reopen it, everything should be all set up for you. Good luck with your course!${clear}"
+read -p "${lightblue}Hit return to close the Terminal app. Once you reopen it, everything should be all set up for you. Now, go make the most of your time at V School!${clear}"
 
 osascript -e "do shell script \"osascript -e \\\"tell application \\\\\\\"Terminal\\\\\\\" to quit\\\" &> /dev/null &\""; exit
